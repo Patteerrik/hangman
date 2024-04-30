5689ec8c84dfa40af8a8e218c5384d6ec153124f
Patteerrik . <patrik.akerbergz@gmail.com>
2024-04-30 08:18:21 +0000

    created a new file list.py and added content to  it

---
+++ list.py
@@ -0,0 +1,35 @@
+words = [
+
+     "black",
+     "blue",
+     "red",
+     "yellow",
+     "apple",
+     "banana",
+     "orange",
+     "kiwi",
+     "pineapple",
+     "fun",
+     "mistaken",
+     "happy",
+     "excited",
+     "sadness",
+     "work",
+     "vacation",
+     "travel",
+     "hard",
+     "easy",
+     "book",
+     "movie"
+     "podcast",
+     "radio",
+     "sweden",
+     "europe",
+     "asia",
+     "north",
+     "south",
+     "west",
+     "east",
+     "farewell"
+
+]
